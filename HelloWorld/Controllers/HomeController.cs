a42b5e52b21a7680276a45884738956ded0c37a0
Ruslan Albu <ruslan.albu@kurtosys.com>
2013-08-12 09:22:26 +0100

    orchard issue

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Orchard.DisplayManagement;
+using Orchard.Themes;
+
+namespace HelloWorld.Controllers
+{
+    [Themed]
+    public class HomeController : Controller
+    {
+        private readonly dynamic _shapeFactory;
+
+        [Themed(false)]
+        public ActionResult Index()
+        {
+            return View(_shapeFactory.MyShape(
+                Foo: 42,
+                Bar: "baz"
+                ));
+
+            //return View("HelloWorld");
+        }
+
+        public HomeController(IShapeFactory shapeFactory)
+        {
+            _shapeFactory = shapeFactory;
+        }
+        
+    }
+}
